412d009a1af303d4d623541d8b79536a9a122cbf
Nur Faidah Rahmah <60200124046@gmail.com>
2026-07-09 02:23:03 +0800

    docs: add api blueprint documentation and initial master seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,14 +4,33 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $categories = [
+            [
+                'name' => 'Mentoring & Akademik',
+                'description' => 'Bimbingan belajar, tugas kuliah, persiapan ujian, atau sertifikasi.'
+            ],
+            [
+                'name' => 'IT & Coding',
+                'description' => 'Pembuatan website, perbaikan bug, atau instalasi perangkat lunak.'
+            ],
+            [
+                'name' => 'Desain & Kreatif',
+                'description' => 'Pembuatan logo, editing video, atau desain UI/UX Figma.'
+            ],
+            [
+                'name' => 'Jasa Logistik Ringan',
+                'description' => 'Mengantar barang belanjaan, membelikan makanan, atau tugas fisik ringan lainnya.'
+            ],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
     }
 }
